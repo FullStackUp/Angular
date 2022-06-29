10ce143d9a19f863c43155fccc457f4bd571b78b
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-06-29 23:35:27 +0200

    Use structural directive

--- frontend/src/app/faketest.ts
+++ frontend/src/app/faketest.ts
@@ -1,37 +1,7 @@
-import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+<p *ngIf='true'>
+  Ce paragraphe est sur le DOM.
+</p>
 
-@Component({
-  selector: 'my-app',
-  // Interpolation dans le template de nom :
-
-  // Liaison du composant vers l'élément input avec []
-  // Liaison de l'événement input, de l'élément vers le composant, avec ()
-  // Affichage de la valeur avec l'interpolation {{ }}
-  template: `
-    <input [value]="nom" (input)="nom = $event.target.value" />
-    {{ nom }}
-  `,
-  styles: [
-    `
-      h2 {
-        color: red;
-      }
-    `,
-  ],
-})
-export class AppComponent {
-  nom: string;
-  constructor() {
-    this.nom = ``;
-  }
-}
-
-@NgModule({
-  imports: [BrowserModule],
-
-  // Nous déclarons le composant :
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+<p *ngIf='false'>
+  Ce paragraphe n’est pas sur le DOM.
+</p>
