8b665a449d42a411d101c87dec49e234d9a5f770
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-06-30 04:07:29 +0200

    Update directive

--- frontend/src/app/faketest.ts
+++ frontend/src/app/faketest.ts
@@ -1,7 +1,34 @@
-<p *ngIf='true'>
-  Ce paragraphe est sur le DOM.
-</p>
+//fichier app.ts
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-<p *ngIf='false'>
-  Ce paragraphe n’est pas sur le DOM.
-</p>
+@Component({
+  selector: 'my-app',
+  template: `
+    <div *ngFor="let item of items; let i = index; trackBy: trackById">
+      {{ i + 1 }} - {{ item.name }}
+    </div>
+  `
+})
+export class AppComponent {
+  items: { name: string; id: number }[] = [
+    { name: 'Paul', id: 1 },
+    { name: 'Julie', id: 2 },
+    { name: 'Jean', id: 3 }
+  ];
+
+  trackById(index: number, item: { name: string; id: number }) {
+    return item.id;
+  }
+}
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+/*Résultat :
+1 - Paul
+2 - Julie
+3 - Jean
